4b01776f7c588eb6e4406718e3038b6130723291
hinnefe2 <henry.hinnefeld@gmail.com>
2016-05-20 14:43:54 -0500

    adding current in/output to bnc2110 driver

--- labdrivers/ni/bnc2110.py
+++ labdrivers/ni/bnc2110.py
@@ -24,8 +24,8 @@ class bnc2110:
         PyDAQmx.DAQmxResetDevice(self.device)
     
     
-    def setOutput(self, channel='ao1', output=0):
-        """Set the output level of `channel` to `output`
+    def setVoltageOutput(self, channel='ao1', output=0):
+        """Set the voltage output level of `channel` to `output`
         
         Args:
             channel(str): the name of the channel to use
@@ -62,8 +62,46 @@ class bnc2110:
         task.StopTask()
         
         
-    def readInput(self, channel='ai1', minVal=-10.0, maxVal=10.0):
-        """Read the input level of `channel`
+    def setCurrentOutput(self, channel='ao1', output=0):
+        """Set the current output level of `channel` to `output`
+        
+        Args:
+            channel(str): the name of the channel to use
+            output(float): the value to output"""
+        
+        task = Task()
+
+        # from NI-DAQmx C Reference:
+        # int32 DAQmxCreateAOCurrentChan (TaskHandle taskHandle, 
+        #                                 const char physicalChannel[],
+        #                                 const char nameToAssignToChannel[], 
+        #                                 float64 minVal, 
+        #                                 float64 maxVal, 
+        #                                 int32 units, 
+        #                                 const char customScaleName[]);
+        task.CreateAOCurrentChan('/{}/{}'.format(self.device, channel),
+                                 '',
+                                 -10.0,
+                                 10.0,
+                                 PyDAQmx.DAQmx_Val_Amps,
+                                 None)
+
+        task.StartTask()
+        # from NI-DAQmx C Reference:
+        # int32 DAQmxWriteAnalogScalarF64 (TaskHandle taskHandle, 
+        #                                  bool32 autoStart, 
+        #                                  float64 timeout, 
+        #                                  float64 value, 
+        #                                  bool32 *reserved); 
+        task.WriteAnalogScalarF64(1,
+                                  10.0,
+                                  output,
+                                  None)
+        task.StopTask()
+
+
+    def readVoltageInput(self, channel='ai1', minVal=-10.0, maxVal=10.0):
+        """Read the voltage input level of `channel`
         
         Using narrow bounds (with minVal and maxVal) will improve accuracy
         since the same digital resolution will be applied to a smaller
@@ -111,3 +149,59 @@ class bnc2110:
         task.StopTask()
 
         return measured_point.value
+
+
+    def readCurrentInput(self, channel='ai1', minVal=-10.0, maxVal=10.0):
+        """Read the current input level of `channel`
+        
+        Using narrow bounds (with minVal and maxVal) will improve accuracy
+        since the same digital resolution will be applied to a smaller
+        analog range.
+        
+        Args:
+            channel(str): the name of the channel to use
+            minVal(float): the minimum value you expect to measure
+            maxVal(float): the maximum value you expect to measure"""
+        
+        task = Task()
+        measured_point = ctypes.c_double(0)
+        
+        # from NI-DAQmx C Reference:
+        # int32 DAQmxCreateAICurrentChan (TaskHandle taskHandle, 
+        #                                 const char physicalChannel[],
+        #                                 const char nameToAssignToChannel[],
+        #                                 int32 terminalConfig, 
+        #                                 float64 minVal, 
+        #                                 float64 maxVal, 
+        #                                 int32 units, 
+        #                                 int32 shuntResistorLoc, 
+        #                                 float64 extShuntResistorVal, 
+        #                                 const char customScaleName[]);
+        task.CreateAICurrentChan('/{}/{}'.format(self.device, channel),
+                                 '',
+                                 PyDAQmx.DAQmx_Val_Cfg_Default,
+                                 -10.0,
+                                 10.0,
+                                 PyDAQmx.DAQmx_Val_Amps,
+                                 PyDAQmx.DAQmx_Val_Default,
+                                 1.0,
+                                 1.0)
+
+
+        task.StartTask()
+        # from NI-DAQmx C Reference:
+        # int32 DAQmxReadAnalogScalarF64 (TaskHandle taskHandle, 
+        #                                 float64 timeout, 
+        #                                 float64 *value, 
+        #                                 bool32 *reserved);
+        task.ReadAnalogScalarF64(1.0,
+                                 ctypes.POINTER(ctypes.c_double)(measured_point),
+                                 None)
+        
+        # see http://stackoverflow.com/questions/1413851/expected-lp-c-double-instance-instead-of-c-double-array-python-ctypes-error
+        # for an explanation of the POINTER black magic
+        
+        task.StopTask()
+
+        return measured_point.value
+
